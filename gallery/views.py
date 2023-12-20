5633e71ed9b389220bda51608df21f073c32a9a0
m4l0n6 <mahonglonghg123@gmail.com>
2023-12-20 16:15:36 +0700

    sort category

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,5 @@
+
+from typing import Any
 from django.views import generic #Sử dụng module generic để xử lí các yêu cầu HTTP
 from django.urls import reverse_lazy
 
@@ -8,7 +10,17 @@ class Home(generic.ListView):
     model = Gallery 
     template_name = 'home.html' #Trả về template home.html
     queryset = Gallery.objects.all() #Lấy tất cả các đối tượng trong model Gallery  
-    
+    def get_queryset(self): 
+        queryset = super().get_queryset()
+        category = self.request.GET.get('category', None)
+        if category:
+            queryset= queryset.filter(category__title=category)
+        return queryset
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category = self.request.GET.get('category',None)
+        context['selected_category']= category if category else "All"
+        return context
 class UploadImage(generic.CreateView):
     model = Gallery
     template_name = "upload-image.html" #Trả về template upload-image.html
