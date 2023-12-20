6a0f8bd07529bd3d8bec4b5b344e06f36b907ae6
daFoggo <ntgiang141105@gmail.com>
2023-12-20 21:27:38 +0700

    delete static image file, fixed bugs, write some comments

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,3 @@
-
-from typing import Any
 from django.views import generic #Sử dụng module generic để xử lí các yêu cầu HTTP
 from django.urls import reverse_lazy
 
@@ -10,25 +8,28 @@ class Home(generic.ListView):
     model = Gallery 
     template_name = 'home.html' #Trả về template home.html
     queryset = Gallery.objects.all() #Lấy tất cả các đối tượng trong model Gallery  
-    def get_queryset(self): 
+    
+    def get_queryset(self):
         queryset = super().get_queryset()
         category = self.request.GET.get('category', None)
         if category:
-            queryset= queryset.filter(category__title=category)
+            queryset = queryset.filter(category__title=category)
         return queryset
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        category = self.request.GET.get('category',None)
-        context['selected_category']= category if category else "All"
+        category = self.request.GET.get('category', None)
+        context['selected_category'] = category if category else "All"
         return context
+    
 class UploadImage(generic.CreateView):
     model = Gallery
     template_name = "upload-image.html" #Trả về template upload-image.html
     form_class = GalleryForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('home') #Quay về trang home.html sau khi upload thành công
     
 class CreateCategory(generic.CreateView):
     model = Category
     template_name = "create-category.html" #Trả về template create-category.html
     form_class = CategoryForm
-    success_url = reverse_lazy('upload-image')
+    success_url = reverse_lazy('upload-image') #Quay về trang upload-image.html sau khi tạo category thành công
